67f456900d4cca55920769bb8653736f74865ff1
ziaahsan <ahsan.zia@mail.utoronto.ca>
2020-08-20 01:22:13 -0400

    Added global shortcut

--- ex/main/window/create.js
+++ ex/main/window/create.js
@@ -1,23 +1,30 @@
 "use strict";
 const isDev = require('electron-is-dev');
-
+const { globalShortcut } = require('electron')
 const { BrowserWindow } = require('glasstron')
 const path = require('path')
+
+let mainWindow = null
+let hidden = false
 
 module.exports = (app) => {
   app.commandLine.appendSwitch("enable-transparent-visuals")
   app.whenReady().then(() => {
     createWindow()
     function createWindow ()  {
-      const mainWindow = new BrowserWindow({
+      mainWindow = new BrowserWindow({
+        backgroundColor: "#00000000",
+        
         frame: false,
         transparent: true,
         resizable: true,
+
+        // skipTaskbar: true,
   
         blur: true,
         blurType: "blurbehind",
         vibrancy: "fullscreen-ui",
-      
+
         webPreferences: {
           nodeIntegration: false,
           preload: path.join(__dirname, 'preload.js'),
@@ -26,19 +33,37 @@ module.exports = (app) => {
   
       mainWindow.loadFile('public/index.html')
       mainWindow.maximize()
+
+      // Register a 'ctrl+space' shortcut listener.
+      let ret = globalShortcut.register('ctrl+space', () => {
+        if (!hidden) {
+          mainWindow.hide()
+        } else {
+          mainWindow.show()
+        }
+
+        hidden = !hidden
+      })
+      if ((!ret || !globalShortcut.isRegistered('ctrl+space')) && isDev)
+        console.log('ctrl+space shortcut failed!')
   
       if (isDev) {
         mainWindow.webContents.openDevTools()
       }
     }
-    
+
     app.on('activate', function () {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
       if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
   })
-    
+  
+  app.on('will-quit', () => {
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
+  })
+
   // Quit when all windows are closed, except on macOS. There, it's common
   // for applications and their menu bar to stay active until the user quits
   // explicitly with Cmd + Q.
